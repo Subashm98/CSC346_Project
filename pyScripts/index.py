c2ed957a78d9f9dcd112e4d67fec6e71cc71a623
UALGuest <ualguest@ICMAC617.local>
2019-03-29 16:54:34 -0700

    coverted html to python

---
+++ pyScripts/index.py
@@ -0,0 +1,298 @@
+#! /usr/bin/env python3
+import cgi
+
+
+
+
+print("Content-Type: text/html")
+print """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reddit</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
+        <div class="navigation-bar">
+                <nav>
+                    <ul id = "nav-ul">
+                            <li id = "navLeft"><img id = "logo" src="logo.png"></li>
+                            <li id = "navLeft"><a href="default.asp">Home</a></li>
+                            <li id = "navLeft"><a href="news.asp">Contact</a></li>
+                            <li id = "navRight"><img id = "userImg" src="default_logImg.png"></li>
+                            <li id = "navRight"><a href="contact.asp">Login</a></li>
+                            <li id = "navRight"><a href="about.asp">Register</a></li>
+                            
+                    </ul>
+                </nav>
+                
+        </div>
+ 
+    <div class = "container">
+        <div class = "h">
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>   
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+                <p>Scrolling text</p>
+        </div>
+
+    </div>
+
+</body>
+</html>
+
+
+
+
+"""
+
+
+
+
