41e3cebdd2cea3f9ffd2cbf688935e638037d0e0
EC2 Default User <ec2-user@ip-172-31-90-52.ec2.internal>
2019-04-02 02:23:31 +0000

    Database now saves correctly

--- testing/login.py
+++ testing/login.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python3
 
-
 import cgi
+
+import cgitb
+cgitb.enable()
+
+import MySQLdb
 
 print("Content-Type: text/html")
 print()
@@ -9,8 +13,37 @@ print()
 
 form = cgi.FieldStorage()
 
+
+conn = MySQLdb.connect(host = "csc346-db.cszwdclaffoa.us-east-1.rds.amazonaws.com",
+		       user = "CSC346_SC",
+		       passwd = "S123456789C",
+		       db = "csc346db")
+
+cursor = conn.cursor()
+cursor.execute("""INSERT INTO users(username,password) VALUES(%s,%s);""", (form["username"].value, form["password"].value))
+cursor.close()
+
+conn.commit()
+
 if "username" in form:
 	print("""<h1>Hello</h1>""")
 if "password" in form:
 	print("""<h1>%s</h1>""" % form["password"].value)
 
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM users;")
+
+print("<br><br><h2>List of accounts:</h2>")
+print("<ul>")
+
+for account in cursor.fetchall():
+		print("<li> user:%s , pass:%s </li>" % (account[0],account[1]))
+
+print("</ul>")
+
+cursor.close()
+
+print("""<a href="http://ec2-34-238-122-238.compute-1.amazonaws.com/cgi-bin/script.py">Back</a>""")
+
+conn.close()
