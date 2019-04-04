de8725c85a7a4f882e5e61fb337b45fa8bba393e
UALGuest <ualguest@ICMAC618.local>
2019-04-04 15:52:22 -0700

    testing

--- cgi-bin/auth.py
+++ cgi-bin/auth.py
@@ -43,6 +43,7 @@ def create_database(conn):
 		cursor.execute("""CREATE TABLE post (
 						  post_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
 						  user_name		VARCHAR (30) NOT NULL,
+						  title			VARCHAR (30) NOT NULL,
 						  msg_as_html	VARCHAR (256) NOT NULL,
 						  likes			INT	UNSIGNED NOT NULL,
 						  CONSTRAINT PK_POST PRIMARY KEY (post_id),
