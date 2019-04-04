9a00441bd0517eda99a3edb74676381537cb6dfd
UALGuest <ualguest@ICMAC618.local>
2019-04-04 12:00:14 -0700

    testing

--- testing/login.py
+++ testing/login.py
@@ -18,11 +18,11 @@ def print_users(cursor):
 
 	try:
 		pwd = cursor.execute("""SELECT password FROM user WHERE user_name = \"%s\";""" % x)
-		print("First pwd print")
 		pwd = cursor.fetchall()
-		print(pwd)
+		
 		pwdResult = [ptuple[0] for ptuple in pwd]
-		print(pwdResult)
+		pWord = pwdResult[0]
+		print(pWord)
 	except Exception as e:
 		print("In exception " + x)
 		print(e)
