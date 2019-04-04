5b640a1bd0bce107a047e1174cac3c6454968c6a
UALGuest <ualguest@ICMAC618.local>
2019-04-03 18:23:40 -0700

    Auth connected to database

--- cgi-bin/auth.py
+++ cgi-bin/auth.py
@@ -10,23 +10,23 @@ cgitb.enable()
 import MySQLdb
 from secret import secret
 
-def main():
-	num = 123
-	al  = "como"
-	p = "hello"
-	u  = "hello"
-	#form = cgi.FieldStorage()
-	form = cgi.FieldStorage()
-	if(form.getvalue("uname") and form.getvalue("psw")):
-		if(form["uname"].value == u and form["psw"].value == p):
-			print("""<body onLoad="location.href='index.py'"><h1>Hurray you got in</h1>""")
-			#execfile("loginPage.py")
-			#print("""<p onLoad="index.py"></p>""")			
-		else:
-			print("""<body onLoad="location.href='loginPage.py'"></body>""")
-			print("<h1>Wrong</h1>")
-	else:
-		print("<h1>OH NO something went wrong</h1>")
+# def main():
+# 	num = 123
+# 	al  = "como"
+# 	p = "hello"
+# 	u  = "hello"
+# 	#form = cgi.FieldStorage()
+# 	form = cgi.FieldStorage()
+# 	if(form.getvalue("uname") and form.getvalue("psw")):
+# 		if(form["uname"].value == u and form["psw"].value == p):
+# 			print("""<body onLoad="location.href='index.py'"><h1>Hurray you got in</h1>""")
+# 			#execfile("loginPage.py")
+# 			#print("""<p onLoad="index.py"></p>""")			
+# 		else:
+# 			print("""<body onLoad="location.href='loginPage.py'"></body>""")
+# 			print("<h1>Wrong</h1>")
+# 	else:
+# 		print("<h1>OH NO something went wrong</h1>")
 
 def create_database(conn):
 	user_tbl = 'user'
@@ -41,78 +41,78 @@ def create_database(conn):
 	
 
 #	if (user_tbl not in results or post_tbl not in results or comment_tbl not in results):
+#		cursor.execute("DROP TABLE user;")
+#		cursor.execute("DROP TABLE post;")
+#		cursor.execute("DROP TABLE comment;")
 	if (user_tbl not in results):
 		cursor.execute("""CREATE TABLE user (
-				  user_id	INT UNSIGNED AUTO_INCREMENT,
-				  first_name	VARCHAR (15) NOT NULL,
-				  last_name	VARCHAR (15) NOT NULL,
-				  pass_hash	VARCHAR (15) NOT NULL,
+				  user_name	VARCHAR (15) NOT NULL,
+				  full_name	VARCHAR (15) NOT NULL,
+				  password	VARCHAR (15) NOT NULL,
 				  gender	VARCHAR (7)  NOT NULL,
 				  email		VARCHAR (30) NOT NULL UNIQUE,
-				  phone_number	VARCHAR (11) NOT NULL,
-				  CONSTRAINT PK_USER PRIMARY KEY (user_id)
+				  phone	VARCHAR (11) NOT NULL,
+				  CONSTRAINT PK_USER PRIMARY KEY (user_name)
 				  );
 				  """)
 
 #		cursor.execute("""CREATE TABLE post (
 #				  post_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#				  user_id		INT NOT NULL,
+#				  user_name		INT NOT NULL,
 #				  msg_as_html	VARCHAR (256) NOT NULL,
 #				  CONSTRAINT PK_POST PRIMARY KEY (post_id),
-#				  CONSTRAINT FK_USER_POST FOREIGN KEY (user_id) REFERENCES users (user_id)
+#				  CONSTRAINT FK_USER_POST FOREIGN KEY (user_name) REFERENCES user (user_name)
 #				  );
 #				  """)
 
 #		cursor.execute(CREATE TABLE comment (
 #				  comment_id	INT UNSIGNED NOT NULL AUTO_INCREMENT
 #				  post_id		INT NOT NULL,
-#				  user_id		INT NOT NULL,
+#				  user_name		INT NOT NULL,
 #				  msg_as_html	VARCHAR (256) NOT NULL
 #				  CONSTRAINT PK_COMMENT PRIMARY KEY (comment_id),
 #				  CONSTRAINT FK_COMMENT_POST FOREIGN KEY (post_id) REFERENCES post (post_id),
-#				  CONSTRAINT FK_COMMENT_USER FOREIGN KEY (user_id) REFERENCES user (user_id)
+#				  CONSTRAINT FK_COMMENT_USER FOREIGN KEY (user_name) REFERENCES user (user_name)
 #				  );
 #				  """)
-	
-	
-
 
 	cursor.close()
 
-def pHash(passwrord):
+def pHash(password):
 	#To be Implemented
 	return password
 
-def main2():
+def main():
 	form = cgi.FieldStorage()
 
 	conn = MySQLdb.connect(host = secret.SQL_HOST,
         	               user = secret.SQL_USER,
                 	       passwd = secret.SQL_PASSWD,
-                       	db = secret.SQL_DB
+                       	   db = secret.SQL_DB
 	)
 
 	create_database(conn)
-
+	cursor = conn.cursor()
+	
 	try:
-		cursor = conn.cursor()
-		cursor.execute("""INSERT INTO user (first_name,last_name,pass_hash,gender,email,phone_number) 
-				  VALUES (%s,%s,%s,%s,%s,%s);""", 
-				(form["first_name"].value, form["last_name"].value, 
-				 pHash(form["password"].value), form["gender"].value,
-				 form["email"].value, form["phone_number"].value))
+		cursor.execute("""INSERT INTO user (user_name,full_name,password,gender,email,phone_number) 
+						  VALUES (%s,%s,%s,%s,%s,%s);""", 
+						  (form["user_name"].value, form["full_name"].value, 
+						   pHash(form["password"].value), form["gender"].value,
+						   form["email"].value, form["phone"].value))
 
 		cursor.close()
 		conn.commit()
+		conn.close()
+
+		print("""<body onLoad="location.href='index.py'"><h1>Hurray you got in</h1>""")
 		
 	except Exception as e:
 		conn.rollback()
+		cursor.close()
+		conn.close()
 		print("<h1>An error has occured in adding to the database</h1>")
-		print(e)	
-
-	print("""<a href="./script.py">Back</a>""")
-
-	conn.close()
+		print("""<body onLoad="location.href='loginPage.py'"></body>""")
 
 print("Content-Type:text/html")
 print()
