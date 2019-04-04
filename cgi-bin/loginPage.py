a42853f9da29c11e52486efe56dedcbd74b0ed9e
UALGuest <ualguest@ICMAC618.local>
2019-04-04 12:10:54 -0700

    testing

--- cgi-bin/loginPage.py
+++ cgi-bin/loginPage.py
@@ -413,7 +413,7 @@ def loginDiv():
         <div class="card card-4">
             <div class="card-body">
                 <h2 class="title">Login</h2>
-                <form method="POST">          <!-- <form action="/action_page.php"> -->
+                <form method="POST" action=auth.py>          <!-- <form action="/action_page.php"> -->
                     <div class="imgcontainer">
                         <img src="https://raw.githubusercontent.com/Subashm98/CSC346_Project/master/pyScripts/selfcare_login_pic.png" alt="Avatar" class="avatar">
                     </div>
