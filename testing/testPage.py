a39aa9950bc0ac0eac76669f1e979e01b64069bf
EC2 Default User <ec2-user@ip-172-31-90-52.ec2.internal>
2019-04-24 20:17:06 +0000

    testing cookies

--- testing/testPage.py
+++ testing/testPage.py
@@ -6,16 +6,28 @@ import cgitb
 cgitb.enable()
 
 import os
+import sys
 
 from http import cookies
 
 def main():
-        try:
-                cookie = cookies.SimpleCookie(os.environ["HTTP_COOKIE"])
-                print("sessionID = " + cookie["session"].value)
-        except Exception as e:
-                print(e)
+        #try:
+	cookie = cookies.SimpleCookie()
+	cookie["session"] = "123"
+	print(cookie)
+	print()
+
+	for item in os.environ.keys():
+		print("""%s, %s<br>""" % (item, os.environ[item]))
+		
+	#print("""<a href="./testPage2.py">TP2</a>""")	
+	
+	print("""<body onLoad="location.href='./testPage2.py'"></body>""")
+
+	#cookie = cookies.SimpleCookie(os.environ["HTTP_COOKIE"])
+                #print("sessionID = " + cookie["session"].value)
+        #except Exception as e:
+                #print(e)
                 
-print("Content-Type:text/plain")
-print()
+print("Content-Type: text/html")
 main()
