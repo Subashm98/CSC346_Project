a61b4039a1f0b6a6141c420daea4694a791f0472
Subash Mainali <Subash@Subashs-MacBook-Pro.local>
2019-04-24 20:24:06 -0700

    testing

--- cgi-bin/comment.py
+++ cgi-bin/comment.py
@@ -264,7 +264,7 @@ def printPost(cursor,user):
     # for i in range(10):
     # 	showPost("title %s " % i,"user", " content %s " % i, i)
 
-    cursor.execute("""SELECT * FROM post WHERE user_name = '%s';"""%user)
+    cursor.execute("""SELECT * FROM post WHERE user_name = "%s";"""%user)
     results = cursor.fetchall()
 
     for post in results:
