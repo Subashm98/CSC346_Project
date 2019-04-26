f149732f87be107f8b6eb444cc2784202b0d9817
Subash Mainali <Subash@Subashs-MacBook-Pro.local>
2019-04-26 15:03:30 -0700

    testing

--- cgi-bin/addComment.py
+++ cgi-bin/addComment.py
@@ -304,9 +304,7 @@ def main():
     
     postId  = form["post_id"].value
     #print("""<h1>%s</h1>"""%postId)
-    comment = ""
-    if "comment" in form:
-        comment = form["comment"].value
+    comment = form["comment"].value
     
     cursor.execute("""INSERT INTO comment (post_id,user_name,msg_as_html)
 						VALUES (%s,%s,%s,%s);""",
