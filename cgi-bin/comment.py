b760a0b4cf53559e8d2482649202c32766bdd202
Subash Mainali <Subash@Subashs-MacBook-Pro.local>
2019-04-26 15:55:46 -0700

    testing

--- cgi-bin/comment.py
+++ cgi-bin/comment.py
@@ -247,9 +247,10 @@ def showPost(idd, title,op,cont,likes,imgSrc, postId):
                          <form method="POST">
                             <input type = "hidden" name = "post_id" value = \"%s\"></input> 
                             <div class="box-cell box1"> 
-                               
                                 <button class="btn" type="submit" name="addC" i value="addC">Add Comment</button>
                             </div>
+                            <br>
+                            <br>
                             <div class="box-cell box2"> 
                                 <textarea class="input--style-5" type="text" name="comment"  required></textarea>
                             </div>
