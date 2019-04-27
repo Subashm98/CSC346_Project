ec1d15c4d2e39ebee8801c2f30112606b48bfb49
EC2 Default User <ec2-user@ip-172-31-90-52.ec2.internal>
2019-04-27 01:49:17 +0000

    logout script runs

--- cgi-bin/logout.py
+++ cgi-bin/logout.py
@@ -1,52 +1,35 @@
 #! /usr/bin/env python3
+
 import cgi
+
 import cgitb
 cgitb.enable()
 
 import os
+import sys
 
 from http import cookies
 
 import MySQLdb
 from secret import secret
 
-
 def main():
-    conn = MySQLdb.connect(host = secret.SQL_HOST,
+	conn = MySQLdb.connect(host = secret.SQL_HOST,
         	               user = secret.SQL_USER,
                 	       passwd = secret.SQL_PASSWD,
                        	   db = secret.SQL_DB
 	)
 
-    cursor = conn.cursor()
+	cursor = conn.cursor()
 
-    print("<h1>Hello</h1>")
+	try:
+		cookie = cookies.SimpleCookie(os.environ["HTTP_COOKIE"])
+		print("sessionID = " + cookie["session"].value)
+	except Exception as e:
+		print(e)
+		print("cookie not set")
 
-##    try:
-##        cookie = cookies.SimpleCookie(os.environ["HTTP_COOKIE"])
-##        token = cookie["session"].value
-##
-##        # Check if a session already exists with the current token, delete the session and reload login page if so
-##        cursor.execute("""SELECT sessionID FROM session WHERE sessionID = '%s';""" % token)
-##        results = cursor.fetchall()
-##
-##        tokenResults = [tok[0] for tok in results]
-##
-##        if (token in tokenResults):
-##            cursor.execute("""DELETE FROM session WHERE sessionID = '%s'""" % token)
-##            conn.commit()
-##            cursor.close()
-##            conn.close()
-##		
-##            print("""<body onLoad="location.href='loginPage.py'"></body>""")
-##            
-##    except:
-##        print("""<body onLoad="location.href='loginPage.py'"></body>""")
 
 print("Content-Type: text/html")
 print()
-
-try:
-    main()
-except Exception as e:
-    print(e)
+main()
