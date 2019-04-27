bdb0caaa6d775023c67693dcc4c3fedc5f468102
Andy Nguyen <anguyen5412@email.arizona.edu>
2019-04-26 17:27:05 -0700

    debugging index3

--- testing/index3.py
+++ testing/index3.py
@@ -244,17 +244,14 @@ def showPost(idd, title,op,cont,likes,imgSrc):
                     %s , by: %s
                 </div>
                 <div class="box-row"> 
-                    <form method="POST">
-                        <input type = "hidden" name = "pname" value = \"%s\"></input>
+                    <form method="POST" action="like.py">
+                        <input type = "hidden" name = "post_id" value = \"%s\"></input>
                         
                         <div class="box-cell box1"> 
                                 <label class="label">Likes:%s</label>
                                 <button class="btn" type="submit" name="likeBtn" value="like">Like</button>
                                 <br>
-                                <button class="btn" type="submit" name="disbtn" value="dislike">Dislike</button>
-                                <br>
-                                <br>
-                                <button class="btn" type="submit" name="cmbtn" value="comment">Comments</button>    
+                                <button class="btn" type="submit" name="disbtn" value="dislike">Dislike</button>   
                         </div> 
                         <div class="box-cell box2"> 
                             %s
@@ -264,6 +261,14 @@ def showPost(idd, title,op,cont,likes,imgSrc):
                                
                         </div>
                     </form>
+                    <form method="POST" action="comment.py">
+                        <input type = "hidden" name = "post_id" value = \"%s\"></input>
+                        <input type = "hidden" name = "user_name" value = \"%s\"></input>
+                        <div class="box-cell box1"> 
+                                <button class="btn" type="submit" name="cmbtn" value="comment">Comments</button>    
+                        </div> 
+                    </form>
+
                 </div>                 
             </div> 
         </div>
@@ -271,7 +276,7 @@ def showPost(idd, title,op,cont,likes,imgSrc):
         
     </div>
     <br>   
-    """%(title,op, idd, likes, cont, imgSrc, imgWidth))
+    """%(title,op, idd, likes, cont, imgSrc, imgWidth,idd,op))
 
 def printPost(cursor):
     print("""
